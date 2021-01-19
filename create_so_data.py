4d026e7e30531e9ca76bfc3334e01430e439a6d4
justinpayan <jpayan@cs.umass.edu>
2021-01-18 22:16:49 -0500

    stack overflow data with distrib shift and class incrementality

--- create_so_data.py
+++ create_so_data.py
@@ -1,4 +1,4 @@
-import csv, datetime, json, os, random, re, sys
+import argparse, csv, datetime, json, os, random, re, sys
 import xml.etree.ElementTree as ET
 from so_twokenize import tokenize
 from collections import Counter, defaultdict
@@ -8,10 +8,13 @@ import numpy as np
 
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+
 punkt_param = PunktParameters()
 abbreviation = ['u.s.a', 'fig', 'etc', 'eg', 'mr', 'mrs', 'e.g', 'no', 'vs', 'i.e']
 punkt_param.abbrev_types = set(abbreviation)
 sent_tokenizer = PunktSentenceTokenizer(punkt_param)
+
+np_gen = np.random.default_rng(seed=31415)
 
 
 def get_lines(filename):
@@ -66,22 +69,26 @@ def get_create_date(qid, text, from_xml_tokens):
 def create_json_elts(text, create_date, qid, start_id):
     json_elts = []
     curr_id = start_id
-    new_labels = set()
+    new_labels = []
 
     for l in text:
         if not l[0].startswith("CODE_BLOCK"):
-            json_elts.append( {
+            json_elts.append({
                 "id": curr_id,
                 "sentence": " ".join([s.split("\t")[0] for s in l]),
                 "tag_sequence": " ".join([s.split("\t")[1] for s in l]),
-                "creation_date": create_date.strftime("%Y-%m-%d"),
                 "question_id": qid
             }
             )
+            if create_date is not None:
+                json_elts[-1]["creation_date"] = create_date.strftime("%Y-%m-%d")
             curr_id += 1
 
+            new_labels_here = set()
             for label in [s.split("\t")[1] for s in l]:
-                new_labels.add(label)
+                new_labels_here.add(label)
+            new_labels.append(new_labels_here)
+
     return json_elts, new_labels, curr_id
 
 
@@ -200,15 +207,163 @@ def construct_time_based_dataset(from_xml_tokens, labeled_data_file, out_base, l
 
     if labeled_data_file[-7:] == "dev.txt":
         for idx, split in enumerate(data_splits):
-            with open(out_base + "_%d.json" % (idx+1), 'r') as f:
+            with open(out_base + "_%d.json" % (idx + 1), 'r') as f:
                 train_data_split = json.load(f)
             d_split = train_data_split + data_splits[idx]
-            with open(out_base + "_%d.json" % (idx+1), 'w') as f:
+            with open(out_base + "_%d.json" % (idx + 1), 'w') as f:
                 json.dump(d_split, f)
     else:
         for idx, split in enumerate(data_splits):
-            with open(out_base + "_%d.json" % (idx+1), 'w') as f:
+            with open(out_base + "_%d.json" % (idx + 1), 'w') as f:
                 json.dump(data_splits[idx], f)
+
+    return curr_id, sentence_ct, skipped_annotated, label_set
+
+
+def create_splits(entity_types_to_json_elts, entity_distribs, sorted_entities, total_size):
+    splits = [[], [], [], [], []]
+
+    ct = 0
+    while sum([len(s) for s in splits]) < total_size:
+        if ct % 10000 == 0:
+            print(sum([len(s) for s in splits]))
+            print(total_size)
+        ct += 1
+        for episode in range(5):
+            distrib = entity_distribs[episode, :]
+            # print(distrib)
+
+            draw = np.where(np.random.multinomial(1, distrib))[0][0]
+            # print(draw)
+            entity_to_draw = sorted_entities[draw][0]
+
+            while not len(entity_types_to_json_elts[entity_to_draw]):
+                draw = np.where(np.random.multinomial(1, distrib))[0][0]
+                # print(draw)
+                entity_to_draw = sorted_entities[draw][0]
+
+            def is_forbidden(s, ep):
+                for t in s["tag_sequence"].split(" "):
+                    if ("User_Interface_Element" in t and ep == 0) or \
+                            ("Code_Block" in t and ep > 1) or \
+                            ("Data_Structure" in t and ep < 3):
+                        return True
+                return False
+
+            # Draw one of the samples from there, and remove it
+            for sample in entity_types_to_json_elts[entity_to_draw]:
+                # sample = entity_types_to_json_elts[entity_to_draw][i]
+                if not is_forbidden(sample, episode):
+                    splits[episode].append(sample)
+                    entity_types_to_json_elts[entity_to_draw].remove(sample)
+                    break
+    return splits
+
+
+def load_data(labeled_data_file, lists_of_posts):
+    qid = None
+    lines = get_lines(labeled_data_file)
+    for l in lines:
+        if get_first(l).lower() in ['question_id', 'answer_to_question_id']:
+            next(lines)
+            qid = next(lines).strip().split("\t")[0]
+            lists_of_posts[qid].append([])
+        elif get_first(l).lower() == 'question_url':
+            next(lines)
+            next(lines)
+        elif len(l.strip()) > 0:
+            single_sent = [l]
+            n = next(lines)
+            while len(n.strip()) > 0:
+                single_sent.append(n)
+                n = next(lines)
+            lists_of_posts[qid][-1].append(single_sent)
+
+    print("done loading in annotated data of %s" % labeled_data_file)
+    return lists_of_posts
+
+
+def map_entity_types_to_json_elts(lists_of_posts, label_set, curr_id, sentence_ct):
+    train_data = []
+    # Create a map from entity types to sentences which are available
+    entity_types_to_json_elts = defaultdict(list)
+
+    for qid in lists_of_posts:
+        for text in lists_of_posts[qid]:
+            # text is a list of lists
+            json_elts, new_labels, curr_id = create_json_elts(text, None, qid, curr_id)
+            train_data.extend(json_elts)
+            for local_new_labels, json_elt in zip(new_labels, json_elts):
+                label_set |= local_new_labels
+                for l in local_new_labels:
+                    if l.startswith("B-"):
+                        entity_types_to_json_elts[l[2:]].append(json_elt)
+            sentence_ct += len(text)
+
+    return train_data, entity_types_to_json_elts, label_set, curr_id, sentence_ct
+
+
+def construct_skewed_dataset(labeled_data_files, out_dir, label_set, start_id, sentence_ct, skipped_annotated):
+    lists_of_posts = defaultdict(list)
+    lists_of_posts_test = defaultdict(list)
+
+    # Load in the "train" and "dev" sets, which will be combined into a large train set
+    for labeled_data_file in labeled_data_files:
+        if labeled_data_file[-7:] == "dev.txt" or labeled_data_file[-9:] == "train.txt":
+            lists_of_posts = load_data(labeled_data_file, lists_of_posts)
+        else:
+            lists_of_posts_test = load_data(labeled_data_file, lists_of_posts_test)
+
+    curr_id = start_id
+    train_data, entity_types_to_json_elts, \
+    label_set, curr_id, sentence_ct = map_entity_types_to_json_elts(lists_of_posts, label_set, curr_id, sentence_ct)
+
+    test_data, entity_types_to_json_elts_test, \
+    label_set, curr_id, sentence_ct = map_entity_types_to_json_elts(lists_of_posts_test,
+                                                                    label_set, curr_id, sentence_ct)
+
+    # Get the initial distribution of the entity types
+    # only do this for the training set, and then use the same distribution plus noise to sample test data
+    entity_counts = count_entities(train_data)
+    sorted_entities = sorted(entity_counts.items(), key=lambda x: x[0])
+
+    # Sample the entity type distributions for each episode
+    cts = np.array([e[1] for e in sorted_entities])
+    normalization = np.sum(cts) / 5
+    entity_distribs = np_gen.dirichlet(cts / normalization, 5)
+
+    # Add class incrementality and drops of labels
+    sorted_entity_labels = [e[0] for e in sorted_entities]
+    user_interface_element_idx = sorted_entity_labels.index("User_Interface_Element")
+    code_block_idx = sorted_entity_labels.index("Code_Block")
+    data_structure_idx = sorted_entity_labels.index("Data_Structure")
+    entity_distribs[0, user_interface_element_idx] = 0
+    entity_distribs[0:3, data_structure_idx] = 0
+    entity_distribs[2:, code_block_idx] = 0
+
+    # For each episode, sample from the entity type distribution, and then pull a sentence with that entity type.
+    # Cycle through the episodes so we can get as close to the intended distribution as possible.
+    total_size = len(train_data) / 1.075
+    train_splits = create_splits(entity_types_to_json_elts, entity_distribs, sorted_entities, total_size)
+
+    # Save the train data
+    for idx, split in enumerate(train_splits):
+        with open(os.path.join(out_dir, "so_train_%d.json" % (idx + 1)), 'w') as f:
+            json.dump(split, f)
+
+    # Sample the test data according to a slightly perturbed version of the training data
+    for ep in range(5):
+        entity_distribs[ep, :] = np_gen.dirichlet(entity_distribs[ep, :]+1, 1)
+    entity_distribs[0, user_interface_element_idx] = 0
+    entity_distribs[0:3, data_structure_idx] = 0
+    entity_distribs[2:, code_block_idx] = 0
+
+    total_size = len(test_data) / 1.075
+    test_splits = create_splits(entity_types_to_json_elts_test, entity_distribs, sorted_entities, total_size)
+
+    for idx, split in enumerate(test_splits):
+        with open(os.path.join(out_dir, "so_test_%d.json" % (idx + 1)), 'w') as f:
+            json.dump(split, f)
 
     return curr_id, sentence_ct, skipped_annotated, label_set
 
@@ -262,9 +417,7 @@ def count_entities(data):
     return entity_cts
 
 
-def get_entity_list():
-    data_loc = "/Users/juspayan/Downloads/so_with_code_blocks"
-
+def get_entity_list(data_loc):
     entity_list = set()
     for episode in range(1, 6):
         # for train_test in ["train", "test"]:
@@ -279,7 +432,7 @@ def get_entity_list():
     return sorted(list(entity_list))
 
 
-def plot_entity_pie(entity_cts, episode, train_test, sorted_entities, entity_colors):
+def plot_entity_pie(entity_cts, episode, train_test, sorted_entities, entity_colors, out_base):
     plt.figure(figsize=(8, 8))
     ax = plt.subplot()
 
@@ -287,24 +440,24 @@ def plot_entity_pie(entity_cts, episode, train_test, sorted_entities, entity_col
     # sorted_entities = sorted([i for i in entity_cts if float(entity_cts[i])/total > 0.01])
     # sorted_entities = sorted([i for i in entity_cts])
 
-    sizes = [100*float(entity_cts[i])/total for i in sorted_entities]
+    sizes = [100 * float(entity_cts[i]) / total for i in sorted_entities]
     sorted_entities = [i for i in sorted_entities]
     sorted_entities.append("Other Entities")
-    sizes.append(100-sum(sizes))
+    sizes.append(100 - sum(sizes))
 
-    ax.pie(sizes, colors=[entity_colors[i] for i in sorted_entities], autopct='%1.1f%%')
+    ax.pie(sizes, colors=[entity_colors[i] for i in sorted_entities], autopct='%1.1f%%', labels=sorted_entities)
     ax.axis('equal')
 
     plt.tight_layout(pad=15)
     train_test = "T" + train_test[1:]
     plt.title("%s Ep. %d" % (train_test, episode))
     name = "so_entity_pie_%d_%s" % (episode, train_test)
-    plt.savefig("/Users/juspayan/plots/%s.png" % name)
+    plt.savefig("%s/plots/%s.png" % (out_base, name))
     plt.clf()
 
 
-def plot_all_entity_pies():
-    entity_list = get_entity_list()
+def plot_all_entity_pies(data_loc):
+    entity_list = get_entity_list(data_loc)
 
     random.seed(15)
     all_colors = sorted(mcolors.CSS4_COLORS, key=lambda x: random.random())
@@ -314,18 +467,17 @@ def plot_all_entity_pies():
     print("entity_colors")
     print(entity_colors)
 
-    data_loc = "/Users/juspayan/Downloads/so_with_code_blocks"
     for episode in range(1, 6):
         for train_test in ["train", "test"]:
             with open(os.path.join(data_loc, "so_%s_%d.json" % (train_test, episode)), 'r') as f:
                 data = json.load(f)
             entity_cts = count_entities(data)
             # sorted_entities = sorted(list(entity_cts.keys()))
-            plot_entity_pie(entity_cts, episode, train_test, entity_list, entity_colors)
+            plot_entity_pie(entity_cts, episode, train_test, entity_list, entity_colors, data_loc)
 
 
 def plot_average_percent_over_episodes(up_or_down, entities, train_test, entity_to_percentage_of_ep_1):
-    avgs = [0.0]*5
+    avgs = [0.0] * 5
     for e in entities:
         for idx, p in enumerate(entity_to_percentage_of_ep_1[e]):
             avgs[idx] += p
@@ -359,14 +511,14 @@ def plot_percentage_change_over_episodes():
             entity_cts = count_entities(data)
             total = sum(entity_cts.values())
             for e, c in entity_cts.items():
-                entity_percent_by_episode[e].append(float(c)/total)
+                entity_percent_by_episode[e].append(float(c) / total)
 
         entity_to_percentage_of_ep_1 = defaultdict(list)
 
         for e in entity_percent_by_episode.keys():
             percents = []
             for p in entity_percent_by_episode[e]:
-                percents.append(100*p/entity_percent_by_episode[e][0])
+                percents.append(100 * p / entity_percent_by_episode[e][0])
             entity_to_percentage_of_ep_1[e] = percents
 
         # Get the entities that change at least 20% up or down by the end, and plot those over time
@@ -389,40 +541,54 @@ def plot_percentage_change_over_episodes():
         plot_average_percent_over_episodes("down", entities_down, train_test, entity_to_percentage_of_ep_1)
 
 
-def create_all_datasets():
-    xml_dump_dir = "/Users/juspayan/Downloads/text_files"
+def create_all_datasets(xml_dump_dir, data_dir, out_dir):
     labeled_data_files = ["train.txt", "dev.txt", "test.txt"]
-    labeled_data_files = ["/Users/juspayan/Downloads/StackOverflowNER/resources/annotated_ner_data/StackOverflow/"
-                          + f_name for f_name in labeled_data_files]
-    out_bases = ["so_train", "so_train", "so_test"]
-    out_bases = ["/Users/juspayan/Downloads/" + out_base for out_base in out_bases]
+    labeled_data_files = [os.path.join(data_dir, "StackOverflowNER/resources/annotated_ner_data/StackOverflow",
+                                       f_name) for f_name in labeled_data_files]
     # get_qids_of_interest(labeled_data_files, lists_of_posts_loc)
 
-    from_xml_tokens, skipped_sentences, nonskipped_sentences = load_xml_dump(xml_dump_dir)
+    # from_xml_tokens, skipped_sentences, nonskipped_sentences = load_xml_dump(xml_dump_dir)
 
     curr_id = 0
     label_set = set()
     sentence_ct = 0
     skipped_annotated = 0
-    for labeled_data_file, out_base in zip(labeled_data_files, out_bases):
-        curr_id, sentence_ct, skipped_annotated, label_set = construct_time_based_dataset(from_xml_tokens, labeled_data_file, out_base,
-                                                                       label_set, curr_id, sentence_ct, skipped_annotated)
+
+    # curr_id, sentence_ct, skipped_annotated, label_set = construct_time_based_dataset(from_xml_tokens,
+    #                                                                                   labeled_data_file, out_base,
+    #                                                                                   label_set, curr_id,
+    #                                                                                   sentence_ct,
+    #                                                                                   skipped_annotated)
+    curr_id, sentence_ct, skipped_annotated, label_set = \
+        construct_skewed_dataset(labeled_data_files, out_dir, label_set, curr_id, sentence_ct, skipped_annotated)
 
     print("Number of annotated sentences: %d" % sentence_ct)
     print("Number of annotated sentences skipped: %d" % skipped_annotated)
-    print("Number of sentences skipped in XML dump: %d" % skipped_sentences)
-    print("Number of sentences loaded in XML dump: %d" % nonskipped_sentences)
+    # print("Number of sentences skipped in XML dump: %d" % skipped_sentences)
+    # print("Number of sentences loaded in XML dump: %d" % nonskipped_sentences)
 
     label_map = {}
     for idx, l in enumerate(label_set):
         label_map[l] = idx
 
-    with open("/Users/juspayan/Downloads/so_labels", 'w') as labelf:
+    with open(os.path.join(out_dir, "so_labels"), 'w') as labelf:
         json.dump(label_map, labelf)
 
 
-if __name__ == "__main__":
-    # create_all_datasets()
-    plot_all_entity_pies()
-    # plot_percentage_change_over_episodes()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--labeled_data_dir", default="~/Spring_2021", type=str)
+    parser.add_argument("--xml_dump_dir", default="./so_data", type=str)
+    parser.add_argument("--json_dir", default="./so_data", type=str)
+    args = parser.parse_args()
+    return args
 
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    random.seed(31415)
+
+    create_all_datasets(args.xml_dump_dir, args.labeled_data_dir, args.json_dir)
+    plot_all_entity_pies(args.json_dir)
+    # plot_percentage_change_over_episodes()
