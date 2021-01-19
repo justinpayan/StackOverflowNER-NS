8dd1cc089ceb6c45ea37880cb6f6d60f41e958ca
justinpayan <jpayan@cs.umass.edu>
2021-01-19 01:07:51 -0500

    run finetune and lll

--- submit_setupandrunexp.sh
+++ submit_setupandrunexp.sh
@@ -2,12 +2,25 @@
 
 OUTBASE=/iesl/canvas/jpayan/Lamolrelease
 
-sbatch -J train_so \
-        -e $OUTBASE/logs/train/lamol_train.err \
-        -o $OUTBASE/logs/train/lamol_train.log \
+mkdir -p $OUTBASE/logs/train/lll
+mkdir -p $OUTBASE/logs/train/finetune
+
+sbatch -J lll_so \
+        -e $OUTBASE/logs/train/lll/lamol_train.err \
+        -o $OUTBASE/logs/train/lll/lamol_train.log \
         --mem=15G \
         --partition=gpu \
         --time=01-00:00:00 \
         --gres=gpu:1 \
         ./setupandrunexp.sh 0.25 0.2 6.25e-5 0 "so_1 so_2 so_3 so_4 so_5" so_data/so_labels \
         lll $OUTBASE/models ~/Lamolrelease $OUTBASE
+
+sbatch -J ft_so \
+        -e $OUTBASE/logs/train/finetune/lamol_train.err \
+        -o $OUTBASE/logs/train/finetune/lamol_train.log \
+        --mem=15G \
+        --partition=gpu \
+        --time=01-00:00:00 \
+        --gres=gpu:1 \
+        ./setupandrunexp.sh 0.25 0.2 6.25e-5 0 "so_1 so_2 so_3 so_4 so_5" so_data/so_labels \
+        finetune $OUTBASE/models ~/Lamolrelease $OUTBASE
