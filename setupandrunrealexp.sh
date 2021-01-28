fec4d7435e0e83d8317869d91612fa767a7eca5c
justinpayan <jpayan@cs.umass.edu>
2021-01-28 11:15:14 -0500

    run without tsts

--- setupandrunrealexp.sh
+++ setupandrunrealexp.sh
@@ -14,9 +14,10 @@ python train.py --data_dir $DATA_DIR \
     --tasks $ORDER --n_train_epochs $NUM_EPS --top_k_lm 20 --top_k_ner 20 \
     --lm_lambda $LAM --gen_lm_sample_percentage 0.2 \
     --label_map_file $DATA_DIR/${LABEL_MAP_FILE_NAME} --logging_steps 100 \
-    --learning_rate $LR --use_crf --fp32 --real_sample --add_task_tokens \
-    --use_task_in_ner
-#    --learning_rate $LR --use_crf --fp32 --real_sample &
+    --learning_rate $LR --use_crf --fp32 --real_sample
+#    --learning_rate $LR --use_crf --fp32 --real_sample --add_task_tokens \
+#    --use_task_in_ner
+
 
 
 
