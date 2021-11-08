88e8badd2d156673b03b0d41ba411c0435c731fc
justinpayan <jpayan@cs.umass.edu>
2021-11-08 12:41:50 -0500

    updating so paths are correct, experiment scripts are correct, etc

--- settings.py
+++ settings.py
@@ -410,34 +410,34 @@ TASK_DICT = {
                "n_train_epochs": 25
     },
     "so_1": {
-               "train": os.path.join(args.data_dir, "so_data", "so_train_1.json"),
-               "eval": os.path.join(args.data_dir, "so_data", "so_test_1.json"),
-               "test": os.path.join(args.data_dir, "so_data", "so_test_1.json"),
-               "n_train_epochs": 25
+               "train": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_train_1.json"),
+               "eval": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_1.json"),
+               "test": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_1.json"),
+               "n_train_epochs": 10
     },
     "so_2": {
                "train": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_train_2.json"),
                "eval": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_2.json"),
                "test": os.path.join(args.data_dir, "so_data",  "skewed_splits", "so_test_2.json"),
-               "n_train_epochs": 25
+               "n_train_epochs": 10
     },
     "so_3": {
                "train": os.path.join(args.data_dir, "so_data",  "skewed_splits", "so_train_3.json"),
                "eval": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_3.json"),
                "test": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_3.json"),
-               "n_train_epochs": 25
+               "n_train_epochs": 10
     },
     "so_4": {
                "train": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_train_4.json"),
                "eval": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_4.json"),
                "test": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_4.json"),
-               "n_train_epochs": 25
+               "n_train_epochs": 10
     },
     "so_5": {
                "train": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_train_5.json"),
                "eval": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_5.json"),
                "test": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_test_5.json"),
-               "n_train_epochs": 25
+               "n_train_epochs": 10
     },
     "so_all_1": {
                "train": os.path.join(args.data_dir, "so_data", "skewed_splits", "so_train_all_1.json"),
--- setupandrunrealexp.sh
+++ setupandrunrealexp.sh
@@ -1,6 +1,4 @@
-source activate pytorch_p36
-conda deactivate
-conda activate pytorch_p36
+#!/usr/bin/env bash
 
 LAM=$1
 LR=$2
@@ -9,21 +7,10 @@ ORDER=$4
 LABEL_MAP_FILE_NAME=$5
 MODEL_DIR_ROOT=$6
 DATA_DIR=$7
-# use --real_sample to run the experiment with real data instead of generated data.
 
 python train.py --data_dir $DATA_DIR \
     --model_dir_root $MODEL_DIR_ROOT --seq_train_type lll \
     --tasks $ORDER --n_train_epochs $NUM_EPS --top_k_lm 20 --top_k_ner 20 \
     --lm_lambda $LAM --gen_lm_sample_percentage 0.2 \
     --label_map_file $DATA_DIR/${LABEL_MAP_FILE_NAME} --logging_steps 100 \
-    --learning_rate $LR --use_crf --fp32 --real_sample --add_task_tokens \
-    --use_task_in_ner
-#    --learning_rate $LR --use_crf --fp32 --real_sample &
-
-
-
-
-# ./setupandrunrealexp.sh 0.25 6.25e-5 0 "conll_eng wnut" wnut_conll_labels
-# --add_task_tokens --use_task_in_ner
-# ./setupandrunrealexp.sh 0.25 6.25e-5 0 "so_1 so_2 so_3 so_4 so_5" so_episodes_dataset/so_labels
-
+    --learning_rate $LR --use_crf --fp32 --real_sample --min_n_steps 10000 --min_batch_size 50
